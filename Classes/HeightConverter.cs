d8aacbdea3f56a1f31044fe45cb1cd5b4cb36643
Karina <nikkarolin@yandex.ru>
2024-05-23 06:20:00 +0500

    фикс интерфейса

---
+++ Classes/HeightConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MyGame.Classes
+{
+    public class HeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double windowHeight = (double)value;
+            return windowHeight - 100; // Вычитаем 100 пикселей, чтобы оставить место для других элементов
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
